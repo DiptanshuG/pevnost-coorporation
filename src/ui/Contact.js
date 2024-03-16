2b56c7776c7eb3424af8a8e306ee96b3b4375b1b
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2024-03-17 01:03:23 +0530

    update

--- src/ui/Contact.js
+++ src/ui/Contact.js
@@ -19,16 +19,15 @@ const Contact = () => {
               <div className="col-12" data-aos="fade-up" data-aos-delay="100">
                 <div className="info-item d-flex flex-column flex-lg-row justify-content-lg-between">
                   <div>
-                    <i className="bi bi-geo-alt" style={{ color: "#2b180d", background:"#905E26" }}></i>
+                    <i className="bi bi-geo-alt" style={{ color: "white", background:"#905E26" }}></i>
                     <div>
                       <h4 style={{ color: "#2b180d" }}>Location:</h4>
-                      <h5 style={{ color: "#2b180d" }}>Address:</h5>
                       <p style={{ color: "#2b180d" }}>42 SDX Minal Residency, Bhopal, Madhya Pradesh, India - 462023</p>
                     </div>
                   </div>
                   
                   <div>
-                    <i className="bi bi-envelope" style={{ color: "#2b180d" ,background:"#905E26" }}></i>
+                    <i className="bi bi-envelope" style={{  color: "white" ,background:"#905E26" }}></i>
                     <div>
                       <h4 style={{ color: "#2b180d" }}>Email:</h4>
                       <p style={{ color: "#2b180d" }}><a href="mailto:pevnostcorporation@gmail.com" style={{ color: "#2b180d" }}>pevnostcorporation@gmail.com</a></p>
@@ -36,7 +35,7 @@ const Contact = () => {
                   </div>
                   
                   <div>
-                    <i className="bi bi-phone" style={{ color: "#2b180d",background:"#905E26"  }}></i>
+                    <i className="bi bi-phone" style={{ color: "white",background:"#905E26"  }}></i>
                     <div>
                       <h4 style={{ color: "#2b180d" }}>Mobile:</h4>
                       <p style={{ color: "#2b180d" }}>+91 9302955692</p>
